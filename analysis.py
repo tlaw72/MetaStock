340aad633613580206e191255f4c6afe8b0048a8
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2023-10-25 12:29:27 -0700

    Scatter Plots by Year

--- analysis.py
+++ analysis.py
@@ -11,19 +11,23 @@ def loadData(file):
 def findAvgCloseByMonth(df):
     df['Date'] = pd.to_datetime(df["Date"]).dt.strftime('%Y-%m')
     df_avg = df.groupby('Date')['Close'].mean().reset_index(name='Average_Close')
+    df_avg["Year"] = pd.to_datetime(df_avg['Date'], format='%Y-%m').dt.year
     return df_avg
 
-def plotAvgs(df):
-    df.plot(kind='scatter', x="Date", y="Average_Close", title='Avg monthly META stock closing price')
 
-    plt.savefig('monthlyStockAvg.png')
-    plt.show()
-    # df.plot()
+def plotAvgs(df):
+    for year, group in df.groupby('Year'):
+        plt.figure()
+        group.plot(kind='scatter', x='Date', y='Average_Close', title=f'Avg {year} monthly META stock closing price')
+        plt.xticks(ticks=group['Date'], labels=pd.to_datetime(group['Date'], format='%Y-%m').dt.strftime('%B').tolist(), rotation=30)
+        plt.savefig(f'{year}monthlyStockAvg.png')
+        plt.close()
+ 
 
 def main():
     df = loadData(stock_data_file)
     df = findAvgCloseByMonth(df)
-    print(df.head())
     plotAvgs(df)
 
-main()
+if __name__=="__main__": 
+    main()
