42fb1657d3150b8b77278df8e52e04202183011c
Troy Lawrence <35078371+tlaw72@users.noreply.github.com>
2024-01-10 23:12:06 -0500

    Added 2023 EOY Data and refactored code

--- analysis.py
+++ analysis.py
@@ -29,15 +29,15 @@ def plotYearlyAvgs(df):
         plt.close()
 
 
-def plotOctNovDifference(df):
+def plotMonthDifference(df, monthRange):
 
     # Calculate the difference between average monthly closing stock prices
     # for Oct and Nov and store in new Column
     df["Month"] = pd.to_datetime(df['Date'], format='%Y-%m').dt.month
-    df_fall = df[df["Month"].isin([10,11])]
-    df_fall = df_fall[df_fall['Year']<2023]
+    df_fall = df[df["Month"].isin(monthRange)]
+    df_fall = df_fall[df_fall['Year']<2024]
     df_fall_diff = df_fall.groupby('Year')['Average_Close'].diff().dropna().reset_index(name='Difference')
-    df_fall_diff["index"] = [2012+i for i in range(11)]
+    df_fall_diff["index"] = [2012+i for i in range(12)]
 
     # Plot results
     plt.bar(df_fall_diff["index"], df_fall_diff["Difference"])
@@ -45,15 +45,25 @@ def plotOctNovDifference(df):
         sign = -1.5 if v<0 else 0.75
         plt.text(i+2012, v + sign, "%.2f" %v, ha="center")
     plt.ylim(min(df_fall_diff["Difference"])-3, max(df_fall_diff["Difference"])+3)
-    plt.title(f'Meta\'s Difference in Average Closing Stock Price Between Oct and Nov')
-    plt.savefig(f'plots/OctNovDifferenceGraph.png')
+
+    month1, month2 = "InvalidMonth", "InvalidMonth"
+    match monthRange[0]:
+        case 10:
+            month1, month2 = "Oct", "Nov"
+        case 11:
+            month1, month2 = "Nov", "Dec"
+        case default:
+            print("Pick a valid month range")
+    plt.title(f'Meta\'s Difference in Average Closing Stock Price Between {month1} and {month2}')
+    plt.savefig(f'plots/{month1}{month2}DifferenceGraph.png')
     plt.close()
  
 
 def main():
     df = loadData(stock_data_file)
     df = findAvgCloseByMonth(df)
-    plotOctNovDifference(df)
+    plotMonthDifference(df, [11,12])
+    plotMonthDifference(df, [10,11])
     plotYearlyAvgs(df)
 
 if __name__=="__main__": 
